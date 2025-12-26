212662b07200abaa80f899f160e39c90855469ce
mukha-cmd <mukhammadashurov3@gmail.com>
2025-12-25 23:15:02 -0700

    Passed 389

--- array/findDifference.py
+++ array/findDifference.py
@@ -1,15 +1,18 @@
 def findTheDifference(s: str, t: str) -> str:
     map1 = {}
     map2 = {}
+    if (s == ""):
+        return t
     for char in s:
         map1[char] = map1.get(char, 0) + 1
     for char in t:
         map2[char] = map2.get(char, 0) + 1
-        if (char in s):
-            del(map2[char])
-    key, item = map2.items()
-    print(key, item)
-    return key
+    #print(map1, map2)
+    #print(map1, map2)
+    for key, value in map2.items():
+        if value > map1.get(key, 0):
+            return key
+    #print(map1, map2)
 s = "abcd"
 t = "abcde"
 print(findTheDifference(s, t))
