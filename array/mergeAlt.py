4738cf62d6d364120cbc7ea9441204415a182868
mukha-cmd <mukhammadashurov3@gmail.com>
2025-12-25 22:50:39 -0700

    Passed 1768

--- array/mergeAlt.py
+++ array/mergeAlt.py
@@ -1,10 +1,17 @@
-def mergeAlternately(self, word1: str, word2: str) -> str:
+def mergeAlternately(word1: str, word2: str) -> str:
     word1_count = 0
     word2_count = 0
     merged = ""
-    while (word1_count < len(word1) and word2_count < len(word2)):
-        merged.append(word1[word1_count])
-        merged.append(word2[word2_count])
-        word1_count += 1
-        word2_count += 1
+    for i in range (len(word1) + len(word2)):
+        if (word1_count < len(word1)):
+            merged += word1[word1_count]
+            word1_count += 1
+            i += 1
+        if (word2_count < len(word2)):
+            merged += word2[word2_count]
+            word2_count += 1
+            i += 1
     return merged
+word1 = "ab"
+word2 = "pqrs"
+print(mergeAlternately(word1, word2))
