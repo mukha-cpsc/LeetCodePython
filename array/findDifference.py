37410e4ab3c29bd22a65fc372709eb6dfdee5efc
mukha-cmd <mukhammadashurov3@gmail.com>
2025-12-25 22:58:30 -0700

    Second Attempt to solve 389

--- array/findDifference.py
+++ array/findDifference.py
@@ -1,3 +1,15 @@
 def findTheDifference(s: str, t: str) -> str:
     map1 = {}
     map2 = {}
+    for char in s:
+        map1[char] = map1.get(char, 0) + 1
+    for char in t:
+        map2[char] = map2.get(char, 0) + 1
+        if (char in s):
+            del(map2[char])
+    key, item = map2.items()
+    print(key, item)
+    return key
+s = "abcd"
+t = "abcde"
+print(findTheDifference(s, t))
