2a2ec3dd10f22d88f8e85310c442f2ece304eb16
mukha-cmd <mukhammadashurov3@gmail.com>
2025-12-25 23:30:13 -0700

    Second Attempt to solve 28

--- array/firstOccur.py
+++ array/firstOccur.py
@@ -1,18 +1,27 @@
-def strStr(haystack: str, needle: str) -> int:
-    result = -1
-    j = 0
-    flag = 0
-    for char in haystack:
-        if char == needle[j]:
-            if flag == 0:
-                result = j
-                flag = 1
-            j += 1
-        else:
-            result = -1
-        if (j == len(needle)):
-            return result
-    return result
-haystack = "leetcode"
-needle = "leet"
-print(strStr(haystack, needle))
+class Solution:
+
+
+    def strStr(haystack: str, needle: str) -> int:
+        if (len(needle) == 0):
+            return -1
+        if (len(needle) > len(haystack)):
+            return -1
+        result = -1
+        j = 0
+        flag = 0
+        for i in range(len(haystack)):
+            if haystack[i] == needle[j]:
+                if flag == 0:
+                    result = i
+                    flag = 1
+                j += 1
+            else:
+                result = -1
+            if (j == len(needle)):
+                return result
+        return result
+
+
+haystack = "mississippi"
+needle = "issip"
+print(Solution.strStr(haystack, needle))
